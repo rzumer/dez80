bd92d0ad2d8f9670c9d581ee4c4f7455a61a0170
Raphaël Zumer <rzumer@tebako.net>
2024-02-06 23:14:22 -0500

    Update module visibility
    - Exposes InstructionDecoder, DecodingState, and Instruction
    at the top level of the crate
    - Removes top-level visibility for register data structures
    - Removes public visibility for the decoder module
    
    The goal is to make imports self-documenting by encouraging verbosity,
    while keeping the most important structures accessible at the top level.
    For example, it is not obvious that a dez80::Condition relates to
    a conditional instruction rather than something related to the decoder,
    whereas importing dez80::Instruction::Condition makes the association
    clearer.

--- src/lib.rs
+++ src/lib.rs
@@ -3,10 +3,9 @@
 extern crate strum;
 extern crate strum_macros;
 
-pub mod decoder;
+mod decoder;
 pub mod instruction;
 pub mod register;
 
 pub use decoder::InstructionDecoder;
-pub use instruction::Instruction;
-pub use register::*;
+pub use instruction::{DecodingState, Instruction};
